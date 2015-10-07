2a2dda7090b6f490b9e95f816052e06d044abed6
alexgustafson <alex_gustafson@yahoo.de>
2015-10-07 18:54:44 +0200

    virtual analog synthesis

--- plots/aliasing_example.py
+++ plots/aliasing_example.py
@@ -1,29 +1,36 @@
-import numpy as np
+'''
+This code is adapted from the following URL:
+https://plot.ly/matplotlib/fft/
+'''
+
+from scipy import signal
 import matplotlib.pyplot as plt
+import numpy as np
 
-Fs = 150.0
+Fs = 44100.0
 Ts = 1.0/Fs
+
+
+ff = 5000;  # frequency of the signal
+
 t = np.arange(0,1,Ts)
-
-ff = 5
-y = np.sin(2*np.pi*ff+t)
-
-n = len(y)
+y = signal.sawtooth(2 * np.pi * ff * t)
+n = len(y) # length of the signal
 k = np.arange(n)
 T = n/Fs
-frq = k/T
-frq=[range(n/2)]
-Y = np.fft.fft(y)/n
+frq = k/T # two sides frequency range
+frq = frq[range(n/2)] # one side frequency range
+
+Y = np.fft.fft(y)/n # fft computing and normalization
 Y = Y[range(n/2)]
 
 fig, ax = plt.subplots(2, 1)
-
 ax[0].plot(t,y)
 ax[0].set_xlabel('Time')
 ax[0].set_ylabel('Amplitude')
-#ax[1].plot(frq,abs(Y),'r') # plotting the spectrum
-#ax[1].set_xlabel('Freq (Hz)')
-#ax[1].set_ylabel('|Y(freq)|')
+ax[1].plot(frq,abs(Y),'r') # plotting the spectrum
+ax[1].set_xlabel('Freq (Hz)')
+ax[1].set_ylabel('|Y(freq)|')
 
 
 plt.show()
