49adc27d3455a9e7b08fa1d155ee4cd14ad527b7
alexgustafson <alex_gustafson@yahoo.de>
2015-10-16 12:43:51 +0200

    translation audio over ethernet

--- build.py
+++ build.py
@@ -4,6 +4,8 @@
 import shutil
 import os
 
+# english
+
 os.system("rm main.aux")
 os.system("rm main.bbl")
 os.system("rm main.blg")
@@ -20,3 +22,22 @@ os.system("rm main.aux")
 os.system("rm main.bbl")
 os.system("rm main.blg")
 os.system("rm main.out")
+
+# german
+
+os.system("rm main_de.aux")
+os.system("rm main_de.bbl")
+os.system("rm main_de.blg")
+os.system("rm main_de.out")
+
+os.system("pdflatex main_de.tex")
+os.system("bibtex main_de")
+os.system("pdflatex main_de.tex")
+os.system("pdflatex main_de.tex")
+
+os.system("open main_de.pdf")
+
+os.system("rm main_de.aux")
+os.system("rm main_de.bbl")
+os.system("rm main_de.blg")
+os.system("rm main_de.out")
