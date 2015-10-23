5876e50c11ac22e329a5fceb52960f2321b1a989
alexgustafson <alex_gustafson@yahoo.de>
2015-10-23 10:36:49 +0200

    korrektions chapter 2

--- build.py
+++ build.py
@@ -4,6 +4,27 @@
 import shutil
 import os
 
+os.system("rm main.4ct")
+os.system("rm main.4tc")
+os.system("rm main_de.4ct")
+os.system("rm main_de.4tc")
+os.system("rm main.dvi")
+os.system("rm main_de.dvi")
+os.system("rm main.idv")
+os.system("rm main_de.idv")
+
+os.system("rm main_de.aux")
+os.system("rm main_de.bbl")
+os.system("rm main_de.blg")
+os.system("rm main_de.out")
+os.system("rm main.aux")
+os.system("rm main.bbl")
+os.system("rm main.blg")
+os.system("rm main.out")
+os.system("rm main.out")
+os.system("rm main.out")
+
+
 # english
 
 os.system("pdflatex main.tex")
